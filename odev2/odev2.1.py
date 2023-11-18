688fed70bb34b2d7ea83a70a799e6c615157dc5e
Eren Aksu <121402681+er3nnz@users.noreply.github.com>
2023-11-18 10:19:26 +0300

    Add files via upload

---
+++ odev2/odev2.1.py
@@ -0,0 +1,22 @@
+# x^3 -2x^2 -5= 0 denekleminin [2,4] aralığındaki iterasyonu
+
+def iterayon(x):
+    result = x**3 - 2*x**2 - 5
+    return result
+x=3
+if x==3 and iterayon(x)>0:
+    print("Yeni Iterasyon Araligimiz : [2,3]")
+    if x==2.5 and iterayon(x)>0:
+        print("Yeni Iterasyon Araligimiz : [2,2.5]")
+    else:
+        print("Yeni Iterasyon Araligimiz : [2.5,3]")
+        if x==2.75 and iterayon(x)>0:
+            print("Yeni Iterasyon Araligimiz : [2.5,2.75]")
+        else:
+            print("Yeni Iterasyon Aralıgimiz : [2.75,3]")
+            if x==2.875 and iterayon(x)>0:
+                print("Yeni Iterasyon Araligimiz : [2.875,3]")
+            else :
+                print("Yeni Iterasyon Araligimiz : [2.75,2.875]")
+else:
+    print("Yeni Iterasyon Araligimiz : [3,4]")
